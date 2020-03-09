49d55041ed8b2ed88da54162c1eadc1df19febe4
Peter Siemen <mail@petersiemen.net>
2020-03-09 11:41:43 +0100

    init

---
+++ src/app.js
@@ -0,0 +1,41 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = (event, context, callback) => {
+
+    console.log("hallo");
+
+    event.Records.forEach(function (record) {
+        //Build the S3 Params string.  Watch out here because we're assuming only 1 record per trigger
+        let s3Params = {
+            Bucket: record.s3.bucket.name,
+            Key: record.s3.object.key
+        };
+
+        //Build the dynamoDB connect string using environment variable
+        let dynParams = {
+            TableName: process.env.DYNAMODB_TABLE,
+            Item: {}
+        };
+
+        s3.getObject(s3Params, function (err, data) {
+            if (err) {
+                callback(err);
+            } else {
+                let dynamoData = data.Body.toString().split('|');
+
+                dynamoData.pop(); //get rid of the last entry of the array because its empty
+
+                dynamoData.forEach(function (row) {
+                    dynParams.Item = JSON.parse(row);
+                    docClient.put(dynParams, function (err, data) {
+                        if (err) callback(err);
+                        else callback(null, data);
+                    });
+
+                });
+            }
+        });
+    });
+};
